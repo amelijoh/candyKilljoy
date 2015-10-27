14d23e2f0cc8cce5acec9167412054e7b8929182
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-27 16:34:36 -0400

    Added api parameters

--- candyKilljoy/ViewController.swift
+++ candyKilljoy/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var candyNameInput: UITextField!
     
     
+    
+    let BASE_URL = "https://api.nutritionix.com/v1_1/search/"
+    let PHRASE = "Peanut Butter Cups"
+    let APPID = "8b7e5c3ee11d2b7c21e5d9a563c3ac83"
+    let APPKEY = "8b7e5c3ee11d2b7c21e5d9a563c3ac83"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +33,20 @@ class ViewController: UIViewController {
     @IBAction func candySearchButton(sender: UIButton) {
     }
     
+    
+    //MARK: - API Call
+    
+    func makeAPICall() {
+        
+        let methodArguments: [String: String!] = [
+            "phrase": PHRASE,
+            "appid": APPID,
+            "appKey": APPKEY
+        ]
+        
+        
+        
+    }
 
 }
 
