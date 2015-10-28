35223fb71b0f285b5ba52ef0aa72baac3bde92d2
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-27 23:01:06 -0400

    API call to Nutritionix is working
    Base call function with problems. Search is hardcoded. Also, can't pass max search results. Code will give runtime error if search item has any spaces in it

--- candyKilljoy/ViewController.swift
+++ candyKilljoy/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: UIViewController {
     
     
     let BASE_URL = "https://api.nutritionix.com/v1_1/search/"
-    let PHRASE = "Peanut Butter Cups"
-    let APPID = "8b7e5c3ee11d2b7c21e5d9a563c3ac83"
-    let APPKEY = "8b7e5c3ee11d2b7c21e5d9a563c3ac83"
+    let PHRASE = "twizzler"
+    //let RESULTS = "0%3A5"
+    let APPID = "05fe6213"
+    let APPKEY = "cbe02a040321ae45dbfaf44ef0839621"
     
     
     override func viewDidLoad() {
@@ -31,6 +32,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func candySearchButton(sender: UIButton) {
+        
+        //TO-DO: must write code to parse search string and put underscores in place of spaces or something like that
+        makeAPICall()
     }
     
     
@@ -39,14 +43,113 @@ class ViewController: UIViewController {
     func makeAPICall() {
         
         let methodArguments: [String: String!] = [
-            "phrase": PHRASE,
-            "appid": APPID,
+            "appId": APPID,
             "appKey": APPKEY
         ]
         
+        /* 3 - Get the shared NSURLSession to faciliate network activity */
+        let session = NSURLSession.sharedSession()
+        
+        /* 4 - Create the NSURLRequest using properly escaped URL */
+        let urlString = BASE_URL + "\(PHRASE)" + escapedParameters(methodArguments)
+        //print(urlString)
+        let url = NSURL(string: urlString)!
+        //print(url)
+        let request = NSURLRequest(URL: url)
+        print(request)
         
         
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! [String : AnyObject]
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            print(parsedResult)
+            
+//            /* GUARD: Did Flickr return an error? */
+//            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+//                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+//                return
+//            }
+            
+//            /* GUARD: Is "photos" key in our result? */
+//            guard let photosDictionary = parsedResult["photos"] as? [String:AnyObject] else {
+//                print("Cannot find keys 'photos' in \(parsedResult)")
+//                return
+//            }
+//            let photoArray = photosDictionary["photo"] as! [[String: AnyObject]]
+//            
+//            // I'm assuming this is needed to properly update the class variable holding the photo array
+//            dispatch_async(dispatch_get_main_queue(), {
+//                //self.celebrityPhotoArray =  photoArray
+//                //self.randomSelectNextCelebrityPhoto()
+//            })
+            
+            
+        }
+        
+        task.resume()
     }
+        
+        
+    
+    
+    
+    
+    // MARK: Escape HTML Parameters
+    
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
+    
+    
 
 }
 
