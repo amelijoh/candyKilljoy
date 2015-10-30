866446d35f03ec03f20754329267bc0f75d26a51
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-30 15:37:43 -0400

    Removed prints on viewcontroller

--- candyKilljoy/CandySearchResultTableViewController.swift
+++ candyKilljoy/CandySearchResultTableViewController.swift
@@ -16,26 +16,13 @@ class CandySearchResultTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print("The candy array passed to this view is \(candyNameResults)")
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
     // MARK: - Table view data source
 
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
+
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
